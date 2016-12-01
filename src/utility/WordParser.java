6b803bc4ffe583531dd62f6f7726465ae1b86020
Derek Vonk <derek.vonk@globalorange.nl>
2016-12-01 16:39:59 +0100

    Add method to remove delimiter from String
    add javadocs
    rename variable names for clarity

--- src/utility/WordParser.java
+++ src/utility/WordParser.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WordParser {
@@ -8,18 +9,34 @@ public class WordParser {
     private static String DELIMITER = "\n";
 
     /**
+     * String input is split based on the DELIMITER and adds each split piece as a String to a List.
+     * The first String in the returned List is a concatenation of the entire input string stripped of the DELIMITER.
+     *
      * @param description string to be parsed
      * @return List of Strings representing each delimiter seperated word from description
      */
     public static List<String> parseToWordArray(String description) {
-        List<String> words = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        String cleanDescription = removeDelimiter(description);
+        result.add(cleanDescription);
 
         String[] split = description.split(DELIMITER);
-
         for (String s : split) {
-            words.add(s);
+            result.add(s);
         }
 
-        return words;
+        return result;
     }
+
+    /**
+     * Remove all delimiters from String
+     *
+     * @param description string to be parsed
+     * @return String result of description without the DELIMITER
+     */
+    public static String removeDelimiter(String description) {
+        return description.replace(DELIMITER, "");
+    }
+
 }
