539fab326b32ce15c272f028837da63510bf1c64
DerekVonk <derek.vonk@hva.nl>
2018-01-02 15:26:09 +0100

    add nonce to block.java and add minblock method

--- src/main/Block.java
+++ src/main/Block.java
@@ -11,6 +11,8 @@ public class Block {
     private String data; // our data will be a simple message for now
     private long timestamp; // as a number of milliseconds since 1/1/1970
 
+    private int nonce;
+
     // Constructor
     public Block(String data, String previousHash) {
         this.data = data;
@@ -22,11 +24,19 @@ public class Block {
     // calculate hash from all parts of the block we don't want tampered with
     public String calculateHash() {
         String calculatedHash = StringUtil.applySha256(
-                previousHash + Long.toString(timestamp) + data
+                previousHash + Long.toString(timestamp) + Integer.toString(nonce) + data
         );
 
         return calculatedHash;
     }
 
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
 
 }
