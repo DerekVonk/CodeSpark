1600ff73385b18e4e44664d5c39925f104bb7f5a
DerekVonk <derek.vonk@hva.nl>
2018-01-02 15:27:19 +0100

    update vonkchain class to trigger the mineBlock method for each block add isChainvalid method call

--- src/main/VonkChain.java
+++ src/main/VonkChain.java
@@ -6,40 +6,60 @@ import com.google.gson.GsonBuilder;
 public class VonkChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
 
     public static void main(String[] args) {
 
+        // add our blocks to the blockchain
+
         blockchain.add(new Block("this is the GENESIS block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("adding some more data", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("this is going marvelously", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
         blockchain.add(new Block("on block number 4 now!", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 4... ");
+        blockchain.get(3).mineBlock(difficulty);
+
+        System.out.println("\nVonkchain is valid: " + isChainValid());
 
         String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe VonkChain: ");
         System.out.println(blockChainJson);
     }
 
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
-        // loop through the blockchain to check hashes:
-        for (int i = 0; i < blockchain.size(); i++) {
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
             previousBlock = blockchain.get(i-1);
-
-            // compare registered hash and calculated hash:
-            if (!currentBlock.hash.equals(currentBlock.calculateHash()) ) {
-                System.out.println("Current hashes not equal");
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
                 return false;
             }
-
-            // compare previous hash and registered previous hash:
-            if (!previousBlock.hash.equals(previousBlock.calculateHash()) ) {
-                System.out.println("Previous hashes not equal");
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            //check if hash is solved
+            if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
                 return false;
             }
         }
-
         return true;
     }
 }
