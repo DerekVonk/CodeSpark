225039d2b720095727c0976a64fe443cca667c17
DerekVonk <derek.vonk@hva.nl>
2018-02-06 12:50:32 +0100

    English Beggars setup

---
+++ src/main/EnglishBeggars.java
@@ -0,0 +1,12 @@
+package main;
+
+public class EnglishBeggars {
+
+    public static void main(String[] args) {
+        int[] result = beggars(new int[]{1, 2, 3, 4, 5}, 2);
+    }
+
+    public static int[] beggars(int[] values, int amount) {
+        return new int[]{};
+    }
+}
