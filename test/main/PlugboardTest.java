4dbbc8d4ebfa7027d7ee633f829748b232b501e1
DerekVonk <derek.vonk@hva.nl>
2018-02-07 14:45:33 +0100

    Enigma Machine - part 1: add basic tests

--- test/main/PlugboardTest.java
+++ test/main/PlugboardTest.java
@@ -13,5 +13,19 @@ public class PlugboardTest {
         assertEquals(".", plugboard.process("."));
     }
 
+    @Test(expected = InvalidPlugboardWiresException.class)
+    public void testInvalidPlugboard1() throws InvalidPlugboardWiresException {
+        Plugboard plugboard = new Plugboard("ABC");
+    }
+
+    @Test(expected = InvalidPlugboardWiresException.class)
+    public void testInvalidPlugboard2() throws InvalidPlugboardWiresException {
+        Plugboard plugboard = new Plugboard("ABAD");
+    }
+
+    @Test(expected = InvalidPlugboardWiresException.class)
+    public void testInvalidPlugboard3() throws InvalidPlugboardWiresException {
+        Plugboard plugboard = new Plugboard("ABCDEFGHIJKLMNOPQRSTUV");
+    }
 
 }
