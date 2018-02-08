78540a45ce983b8fbe4b83297355b95d640c7cdc
DerekVonk <derek.vonk@hva.nl>
2018-02-08 09:46:01 +0100

    Street Fighter 2 -Character Selection add tests

--- src/main/SF2CharacterSelection.java
+++ src/main/SF2CharacterSelection.java
@@ -2,4 +2,7 @@ package main;
 
 public class SF2CharacterSelection {
 
+    public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
+        return null;
+    }
 }
--- test/main/SF2CharacterSelectionTest.java
+++ test/main/SF2CharacterSelectionTest.java
@@ -4,8 +4,56 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SF2CharacterSelectionTest {
+
+    String[][] fighters = new String[][]{
+            new String[] { "Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega" },
+            new String[] { "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison" },
+    };
+
     @Test
-    public void testSomething() {
-        // assertEquals("expected", "actual");
+    public void shouldWorkWithNoMoves() {
+        String[] solution = new String[]{};
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[]{0,0}, new String[]{}));
     }
-}
+
+    @Test
+    public void shouldWorkWithFewMoves(){
+        String[] moves = new String[] { "up", "left", "right", "left", "left" };
+        String[] solution = new String[] { "Ryu", "Vega", "Ryu", "Vega", "Balrog" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }
+
+    @Test
+    public void shouldWorkWhenAlwaysMovingLeft(){
+        String[] moves = new String[] { "left", "left", "left", "left", "left", "left", "left", "left" };
+        String[] solution = new String[] { "Vega", "Balrog", "Guile", "Blanka", "E.Honda", "Ryu", "Vega", "Balrog" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }
+
+    @Test
+    public void shouldWorkWhenAlwaysMovingRight(){
+        String[] moves = new String[] { "right", "right", "right", "right", "right", "right", "right", "right" };
+        String[] solution = new String[] { "E.Honda", "Blanka", "Guile", "Balrog", "Vega", "Ryu", "E.Honda", "Blanka" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }
+
+    @Test
+    public void shouldUseAll4DirectionsClockwiseTwice(){
+        String[] moves = new String[] { "up", "left", "down", "right", "up", "left", "down", "right" };
+        String[] solution = new String[] { "Ryu", "Vega", "M.Bison", "Ken", "Ryu", "Vega", "M.Bison", "Ken" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }
+
+    @Test
+    public void shouldWorkWhenAlwaysMovingDown(){
+        String[] moves = new String[] { "down", "down", "down", "down" };
+        String[] solution = new String[] { "Ken", "Ken", "Ken", "Ken" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }
+
+    @Test
+    public void shouldWorkWhenAlwaysMovingUp(){
+        String[] moves = new String[] { "up", "up", "up", "up" };
+        String[] solution = new String[] { "Ryu", "Ryu", "Ryu", "Ryu" };
+        assertEquals(solution, SF2CharacterSelection.streetFighterSelection(fighters, new int[] {0,0}, moves));
+    }    }
