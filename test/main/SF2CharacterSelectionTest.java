57dc651d8e9e2c5e3990a5c6604653844cf5abf8
DerekVonk <derek.vonk@hva.nl>
2018-02-08 09:42:21 +0100

    Street Fighter 2 -Character Selection setup

---
+++ test/main/SF2CharacterSelectionTest.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SF2CharacterSelectionTest {
+    @Test
+    public void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
