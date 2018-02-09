6c89eb95e625bec57c9665c652b8669c482dcde8
DerekVonk <derek.vonk@hva.nl>
2018-02-09 09:24:06 +0100

    Street Fighter 2 - WIP

--- src/main/SF2CharacterSelection.java
+++ src/main/SF2CharacterSelection.java
@@ -1,8 +1,29 @@
 package main;
 
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+import java.util.ArrayList;
+
 public class SF2CharacterSelection {
 
+    private final static int[] UP = {0, 1};
+    private final static int[] DOWN = {0, -1};
+    private final static int[] RIGHT = {1, 0};
+    private final static int[] LEFT = {-1, 0};
+
+    private static final int[][] select = new int[4][{},{},{},{}]
+
     public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
-        return null;
+        ArrayList<String> selectionHistory = new ArrayList<>();
+
+        try {
+            for (String move : moves) {
+
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+
+        return (String[]) selectionHistory.toArray();
+
     }
 }
