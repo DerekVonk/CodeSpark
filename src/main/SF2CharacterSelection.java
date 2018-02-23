47f692b7487601a5c687abf5130131cfada815f0
DerekVonk <derek.vonk@hva.nl>
2018-02-23 16:55:29 +0100

    Street Fighter 2 - 1 test failed

--- src/main/SF2CharacterSelection.java
+++ src/main/SF2CharacterSelection.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class SF2CharacterSelection {
 
-    private static final Map<String, int[]> select = new HashMap();
+    private static final Map<String, int[]> select = new HashMap<>();
 
     static {
         select.put("up", new int[] {0, -1});
@@ -21,28 +21,25 @@ public class SF2CharacterSelection {
         for (String move : moves) {
             int[] selection = select.get(move);
 
-            try {
-                // allow for circular movements on rows
-                if (move.equalsIgnoreCase("LEFT") && position[0] == 0) {
-                    position[0] = fighters[0].length;
-
-                } else if (move.equalsIgnoreCase("RIGHT") && position[0] == fighters[0].length - 1) {
-                    position[0] = -1;
-
-                }
-
-                selectionHistory.add(fighters[position[1] + selection[1]][position[0] + selection[0]]);
-                position[0] += selection[0];
-                position[1] += selection[1];
-
-            // only up or down movements should throw an AIOOBE
-            } catch (ArrayIndexOutOfBoundsException e) {
+            // allow for circular movements on rows
+            if (move.equalsIgnoreCase("DOWN") && position[1] == fighters.length - 1) {
+                selectionHistory.add(fighters[position[1]][position[0]]);
+                continue;
+            } else if (move.equalsIgnoreCase("UP") && position[1] == 0) {
                 selectionHistory.add(fighters[position[0]][position[1]]);
+                continue;
+            } else if (move.equalsIgnoreCase("LEFT") && position[0] == 0) {
+                position[0] = fighters[0].length;
+            } else if (move.equalsIgnoreCase("RIGHT") && position[0] == fighters[0].length - 1) {
+                position[0] = -1;
             }
+
+            selectionHistory.add(fighters[position[1] + selection[1]][position[0] + selection[0]]);
+            position[0] += selection[0];
+            position[1] += selection[1];
 
         }
 
         return selectionHistory.toArray(new String[0]);
-
     }
 }
