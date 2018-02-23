aba7dd1441edd803cd3c0cb8d3d351d118580480
DerekVonk <derek.vonk@hva.nl>
2018-02-23 17:03:48 +0100

    Street Fighter 2 - still  with trycatch 1 test failed

--- src/main/SF2CharacterSelection.java
+++ src/main/SF2CharacterSelection.java
@@ -18,25 +18,32 @@ public class SF2CharacterSelection {
     public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
         ArrayList<String> selectionHistory = new ArrayList<>();
 
+
         for (String move : moves) {
             int[] selection = select.get(move);
 
-            // allow for circular movements on rows
-            if (move.equalsIgnoreCase("DOWN") && position[1] == fighters.length - 1) {
-                selectionHistory.add(fighters[position[1]][position[0]]);
-                continue;
-            } else if (move.equalsIgnoreCase("UP") && position[1] == 0) {
-                selectionHistory.add(fighters[position[0]][position[1]]);
-                continue;
-            } else if (move.equalsIgnoreCase("LEFT") && position[0] == 0) {
-                position[0] = fighters[0].length;
-            } else if (move.equalsIgnoreCase("RIGHT") && position[0] == fighters[0].length - 1) {
-                position[0] = -1;
-            }
+            try {
+                // allow for circular movements on rows
+                if (move.equalsIgnoreCase("DOWN") && position[1] == fighters.length - 1) {
+                    selectionHistory.add(fighters[position[1]][position[0]]);
+                    continue;
+                } else if (move.equalsIgnoreCase("UP") && position[1] == 0) {
+                    selectionHistory.add(fighters[position[0]][position[1]]);
+                    continue;
+                } else if (move.equalsIgnoreCase("LEFT") && position[0] == 0) {
+                    position[0] = fighters[0].length;
+                } else if (move.equalsIgnoreCase("RIGHT") && position[0] == fighters[0].length - 1) {
+                    position[0] = -1;
+                }
 
-            selectionHistory.add(fighters[position[1] + selection[1]][position[0] + selection[0]]);
-            position[0] += selection[0];
-            position[1] += selection[1];
+                selectionHistory.add(fighters[position[1] + selection[1]][position[0] + selection[0]]);
+                position[0] += selection[0];
+                position[1] += selection[1];
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
 
         }
 
