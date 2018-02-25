91dee6903e1b0901644aab46efe82e6719990ee2
Derek Vonk <derek.vonk@hva.nl>
2018-02-25 22:07:41 +0100

    test wip: InvalidAlgorithmParameterException

--- src/sparkchain/SparkChain.java
+++ src/sparkchain/SparkChain.java
@@ -1,17 +1,37 @@
 package sparkchain;
 
+import java.security.Security;
 import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
+import utils.StringUtil;
 
 public class SparkChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
     public static int difficulty = 1;
+    public static Wallet walletA;
+    public static Wallet walletB;
 
     public static void main(String[] args) {
 
-        // add our blocks to the blockchain
+        //Setup Bouncey castle as a Security Provider
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        //Create the new wallets
+        walletA = new Wallet();
+        walletB = new Wallet();
+        //Test public and private keys
+        System.out.println("Private and public keys:");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+        //Create a test transaction from WalletA to walletB
+        Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+        transaction.generateSignature(walletA.privateKey);
+        //Verify the signature works and verify it from the public key
+        System.out.println("Is signature verified");
+        System.out.println(transaction.verifiySignature());
 
+        //testing
+        // add our blocks to the blockchain
         blockchain.add(new Block("this is the GENESIS block", "0"));
         System.out.println("Trying to Mine block 1... ");
         blockchain.get(0).mineBlock(difficulty);
@@ -36,6 +56,7 @@ public class SparkChain {
         String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println("\nThe VonkChain: ");
         System.out.println(blockChainJson);
+
     }
 
     public static Boolean isChainValid() {
--- src/sparkchain/Wallet.java
+++ src/sparkchain/Wallet.java
@@ -14,16 +14,16 @@ public class Wallet {
 
     public void generateKeyPair() {
         try {
-
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
             ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
             // Initialize the key generator and generate a KeyPair
-            keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+            keyGen.initialize(ecSpec, random); //256
             KeyPair keyPair = keyGen.generateKeyPair();
             // Set the public and private keys from the keyPair
             privateKey = keyPair.getPrivate();
             publicKey = keyPair.getPublic();
+
         }catch(Exception e) {
             throw new RuntimeException(e);
         }
