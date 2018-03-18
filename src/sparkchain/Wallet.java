6f40f39ca356734864872ff04a1a2ccd0c44d24d
Derek Vonk <derek.vonk@hva.nl>
2018-03-18 15:42:15 +0100

    vonkchain: add getBalance and sendFunds methods to Wallet

--- src/sparkchain/Wallet.java
+++ src/sparkchain/Wallet.java
@@ -2,30 +2,77 @@ package sparkchain;
 
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wallet {
 
-    public PrivateKey privateKey;
-    public PublicKey publicKey;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
 
-    public Wallet() {
+    private Map<String, TransactionOutput> UTXOs = new HashMap<>(); //only UTXOs owned by this wallet.
+
+    Wallet() {
         generateKeyPair();
     }
 
-    public void generateKeyPair() {
+    private void generateKeyPair() {
         try {
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
             SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
             ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
             // Initialize the key generator and generate a KeyPair
             keyGen.initialize(ecSpec, random); //256
             KeyPair keyPair = keyGen.generateKeyPair();
+
             // Set the public and private keys from the keyPair
             privateKey = keyPair.getPrivate();
             publicKey = keyPair.getPublic();
 
-        }catch(Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    // returns balance and stores the UTXO's owned by this wallet in this.UTXOs
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item : SparkChain.UTXOs.entrySet()) {
+            TransactionOutput UTXO = item.getValue();
+            if (UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+                UTXOs.put(UTXO.id, UTXO); //add it to our list of unspent transactions.
+                total += UTXO.value;
+            }
+        }
+        return total;
+    }
+
+    // Generates and returns a new transaction from this wallet.
+    public Transaction sendFunds(PublicKey recipient, float value) {
+        if (getBalance() < value) { //gather balance and check funds.
+            System.out.println("# Not Enough funds to send transaction. Transaction Discarded.");
+            return null;
+        }
+        //create array list of inputs
+        ArrayList<TransactionInput> inputs = new ArrayList<>();
+
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item : UTXOs.entrySet()) {
+            TransactionOutput UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInput(UTXO.id));
+            if (total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, recipient, value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        for (TransactionInput input : inputs) {
+            UTXOs.remove(input.transactionOutputId);
+        }
+        return newTransaction;
+    }
+
 }
