2ae1934a1bd39715856ab0e9869e841e45430625
derekvonk <derek.vonk@globalorange.nl>
2018-03-28 14:12:59 +0200

    Kata: Are they the "same"? (not attempted yet)

--- src/main/AreSame.java
+++ src/main/AreSame.java
@@ -1,9 +1,22 @@
 package main;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 // Are they the 'same'?
 public class AreSame {
 
     public static boolean comp(int[] a, int[] b) {
-        return false;
+        double[] aAsDoubles = IntStream.of(a)
+                .mapToDouble(i -> i)
+                .sorted()
+                .toArray();
+
+        double[] bAsDoubles = IntStream.of(b)
+                .mapToDouble(Math::sqrt)
+                .sorted()
+                .toArray();
+
+        return Arrays.equals(aAsDoubles, bAsDoubles);
     }
 }
--- test/main/AreSameTest.java
+++ test/main/AreSameTest.java
@@ -2,14 +2,50 @@ package main;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AreSameTest {
 
     @Test
-    void test1() {
+    void testHappyFlow() {
         int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
         int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
         assertEquals(AreSame.comp(a, b), true);
     }
+
+    @Test
+    void testOneEmptyArray() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{};
+        assertEquals(AreSame.comp(a, b), false);
+    }
+
+    @Test
+    void testOtherEmptyArray() {
+        int[] a = new int[]{};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        assertEquals(AreSame.comp(a, b), false);
+    }
+
+    @Test
+    void testOneArrayDiffersInLength() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736};
+        assertEquals(AreSame.comp(a, b), false);
+    }
+
+    @Test
+    void testOneArrayElementIsNotSquareOfAnyNumberInOtherArray1() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{132, 14641, 20736, 361, 25921, 361, 20736, 361};
+        assertEquals(AreSame.comp(a, b), false);
+    }
+
+    @Test
+    void testOneArrayElementIsNotSquareOfAnyNumberInOtherArray2() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 36100, 25921, 361, 20736, 361};
+        assertEquals(AreSame.comp(a, b), false);
+    }
+
 }
