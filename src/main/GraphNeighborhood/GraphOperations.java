859b9ddb4a4bf92064399040e1f5fdfdf186b1c0
derekvonk <derek.vonk@globalorange.nl>
2018-07-12 14:57:28 +0200

    GraphOperations: setup

---
+++ src/main/GraphNeighborhood/GraphOperations.java
@@ -0,0 +1,11 @@
+package main.GraphNeighborhood;
+
+import java.util.Set;
+
+public class GraphOperations {
+
+    public static Set<Vertex> getNeighbours(Graph graph, Vertex vertex){
+        return null;
+        //Have fun coding!
+    }
+}
