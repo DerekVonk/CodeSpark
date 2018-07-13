3d540fbbdecb757761af7fbd24bacdd723905bee
derekvonk <derek.vonk@globalorange.nl>
2018-07-13 14:19:52 +0200

    GraphOperations: add clever best practice result

--- src/main/graphneighborhood/GraphOperations.java
+++ src/main/graphneighborhood/GraphOperations.java
@@ -2,25 +2,34 @@ package main.graphneighborhood;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GraphOperations {
 
     public static Set<Vertex> getNeighbours(Graph graph, Vertex vertex){
-        Set<Vertex> result = new HashSet<>();
-        Set<Edge> edges = graph.getEdges();
-        int vertexUid = vertex.hashCode();
+//        Set<Vertex> result = new HashSet<>();
+//        Set<Edge> edges = graph.getEdges();
+//        int vertexUid = vertex.hashCode();
+//
+//        for (Edge e : edges) {
+//            Vertex v1 = e.getV1();
+//            Vertex v2 = e.getV2();
+//
+//            if (v1.hashCode() == vertexUid) {
+//                result.add(v2);
+//            } else if (v2.hashCode() == vertexUid) {
+//                result.add(v1);
+//            }
+//        }
+//
+//        return result;
 
-        for (Edge e : edges) {
-            Vertex v1 = e.getV1();
-            Vertex v2 = e.getV2();
+        return graph
+                .getEdges()
+                .stream()
+                .filter(x -> x.getV1().equals(vertex) || x.getV2().equals(vertex))
+                .map(x -> x.getV1().equals(vertex) ? x.getV2() : x.getV1())
+                .collect(Collectors.toSet());
 
-            if (v1.hashCode() == vertexUid) {
-                result.add(v2);
-            } else if (v2.hashCode() == vertexUid) {
-                result.add(v1);
-            }
-        }
-
-        return result;
     }
 }
