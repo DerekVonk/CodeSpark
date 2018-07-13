c65b73f8e849215ad9742f79099f7fe78083873d
derekvonk <derek.vonk@globalorange.nl>
2018-07-13 14:15:29 +0200

    GraphOperations: part 1

--- src/main/graphneighborhood/GraphOperations.java
+++ src/main/graphneighborhood/GraphOperations.java
@@ -1,11 +1,26 @@
 package main.graphneighborhood;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class GraphOperations {
 
     public static Set<Vertex> getNeighbours(Graph graph, Vertex vertex){
-        return null;
-        //Have fun coding!
+        Set<Vertex> result = new HashSet<>();
+        Set<Edge> edges = graph.getEdges();
+        int vertexUid = vertex.hashCode();
+
+        for (Edge e : edges) {
+            Vertex v1 = e.getV1();
+            Vertex v2 = e.getV2();
+
+            if (v1.hashCode() == vertexUid) {
+                result.add(v2);
+            } else if (v2.hashCode() == vertexUid) {
+                result.add(v1);
+            }
+        }
+
+        return result;
     }
 }
